258b0eb0cc1e8a73210d6a454f5200e75496635a
Garrett Arant <garrettarant@MacBook-Pro-4.local>
2018-03-22 19:49:35 -0400

    Post Messages and Get Ids

---
+++ Client/src/main/java/ID.java
@@ -0,0 +1,46 @@
+public class ID {
+
+    private String userid;
+    private String name;
+    private String github;
+
+    public ID(String userid, String name, String github) {
+        this.userid = userid;
+        this.name = name;
+        this.github = github;
+    }
+
+    public ID(String name, String github) {
+        this.name = name;
+        this.github = github;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGithub() {
+        return github;
+    }
+
+    public void setGithub(String github) {
+        this.github = github;
+    }
+
+    @Override
+    public String toString(){
+        return "UserID: " + userid + " Name: " + name + " Github: " + github;
+    }
+}
--- Client/src/main/java/YouAreEll.java
+++ Client/src/main/java/YouAreEll.java
@@ -1,13 +1,38 @@
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.request.GetRequest;
+
 public class YouAreEll {
 
     YouAreEll() {
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JsonProcessingException {
         YouAreEll urlhandler = new YouAreEll();
         System.out.println(urlhandler.MakeURLCall("/ids", "GET", ""));
         System.out.println(urlhandler.MakeURLCall("/messages", "GET", ""));
+        urlhandler.postID("G", "Github");
+        urlhandler.postMessage("gjarant", "bell7692", "Is it working?");
     }
+
+    public void postID(String name, String github) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ID idTest = new ID( name, github);
+        String payload = objectMapper.writeValueAsString(idTest);
+        MakeURLCall("/ids", "POST", payload);
+    }
+
+    public void postMessage(String fromId, String toid, String message) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Messages messagesTest = new Messages(fromId, toid, message);
+        String payload = objectMapper.writeValueAsString(messagesTest);
+        String url = "/ids/" + toid + "/messages";
+        MakeURLCall(url, "POST", payload);
+    }
+
 
     public String get_ids() {
         return MakeURLCall("/ids", "GET", "");
@@ -18,6 +43,30 @@ public class YouAreEll {
     }
 
     public String MakeURLCall(String mainurl, String method, String jpayload) {
+        String completeURL = "http://zipcode.rocks:8085" + mainurl;
+        GetRequest request = Unirest.get(completeURL);
+
+        if (method.equals("GET")) {
+            try {
+                return request.asJson().getBody().toString();
+            } catch (UnirestException e) {
+                e.printStackTrace();
+            }
+        }
+        else if (method.equals("POST")){
+            try {
+                return Unirest.post(completeURL).body(jpayload).asJson().getBody().toString();
+            } catch (UnirestException e) {
+                e.printStackTrace();
+            }
+        }
+//        else if (method.equals("PUT")){
+//            try {
+//                return Unirest.put(completeURL).body(jpayload).asJson().getBody().toString();
+//            } catch (UnirestException e) {
+//                e.printStackTrace();
+//            }
+//        }
         return "nada";
     }
 }
